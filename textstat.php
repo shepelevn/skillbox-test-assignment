0c257f30fe4682fdb6eca8464deb12fe1fdc2c20
Nikolay Shepelev <shepelevnikolay97@gmail.com>
2024-05-15 15:49:03 +0300

    Create textstat.php program. Add test files.

---
+++ textstat.php
@@ -0,0 +1,73 @@
+<?php
+
+define("CHUNK_LENGTH", 8192);
+
+if (!isset($argv[1])) {
+    exit("Filename is not provided" . PHP_EOL);
+}
+
+$file = fopen($argv[1], "r");
+
+if (!$file) {
+    exit("File not found" . PHP_EOL);
+}
+
+$countsArray = [];
+$fileSize = 0;
+
+while (!feof($file)) {
+    $chunk = fread($file, CHUNK_LENGTH);
+
+    if ($chunk === false) {
+        exit("Error while reading file");
+    }
+
+    $fileSize += strlen($chunk);
+
+    foreach (mb_str_split($chunk) as $char) {
+        if (!isset($countsArray[$char])) {
+            $countsArray[$char] = 0;
+        }
+
+        $countsArray[$char]++;
+    }
+}
+
+ksort($countsArray);
+
+foreach ($countsArray as $char => $count) {
+    printf("%s - %s%%" . PHP_EOL, charToReadable($char), formatPercentage($count / $fileSize));
+}
+
+function charToReadable(string $char): string
+{
+    return match(mb_ord($char)) {
+        9 => '\t',
+        10 => '\n',
+        12 => '0x12',
+        13 => '0x13',
+        32 => '(space)',
+        default => $char,
+    };
+}
+
+function formatPercentage(float $number): string
+{
+    if ($number >= 0.01) {
+        return strval(round($number, 2));
+    } else {
+        return strval(round($number, findPrecision($number)));
+    }
+}
+
+function findPrecision(float $number): int
+{
+    $precision = 0;
+
+    while ($number < 1) {
+        $number *= 10;
+        $precision++;
+    }
+
+    return $precision;
+}
